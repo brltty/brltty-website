80e0a7209587c9e0e00ae72f4aff1eed9867b503
Dave Mielke <Dave@Mielke.cc>
2026-02-17 08:43:37 -0500

    The current release of brltty is now 6.9.

--- home/brltty-parameters.php
+++ home/brltty-parameters.php
@@ -2,6 +2,6 @@
    define('BRLTTY_COPYRIGHT_HOLDER', 'The BRLTTY Developers');
    define('BRLTTY_COPYRIGHT_YEARS', '1995-2025');
 
-   define('BRLTTY_CURRENT_RELEASE', '6.8');
-   define('BRLAPI_CURRENT_RELEASE', '0.8.7');
+   define('BRLTTY_CURRENT_RELEASE', '6.9');
+   define('BRLAPI_CURRENT_RELEASE', '0.8.8');
 ?>
